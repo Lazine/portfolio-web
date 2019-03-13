6db5f2a13a6aedaa318a69f3b876e14328d39f07
Lazine <huifenhuang648@gmail.com>
2019-03-13 16:27:39 +0800

    mobile hover test

--- js/all.js
+++ js/all.js
@@ -9,3 +9,21 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "-80px";
   }
 }
+
+//hover for mobile
+
+let hovermobile = document.querySelector('.hovertop');
+
+hovermobile.addEventListener('touchstart',touchStart,false);
+hovermobile.addEventListener('touchmove',touchMove,false);
+hovermobile.addEventListener('touchend',touchEnd,false);
+
+
+function touchStart(event) {
+  hovermobile.addClass(".hovertop-mobile"+".hovertop-mobile h3");
+}
+
+function touchEnd(event) {
+  hovermobile.removeClass(".hovertop-mobile"+".hovertop-mobile h3");
+}
+
